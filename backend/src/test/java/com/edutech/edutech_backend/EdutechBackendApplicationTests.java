eaf7f08b79193751373de54c560461514646b23a
Matteo Cuervo <matteocuervo@Matteos-MacBook-Pro.local>
2026-03-08 16:24:30 -0400

    Initial Backend Setup

---
+++ backend/src/test/java/com/edutech/edutech_backend/EdutechBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edutech.edutech_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdutechBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
